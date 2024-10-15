384d28afd37f50d15675623f86ff0c3857fccece
Emilien Giraud <emilien.giraud@etudiant.univ-lr.fr>
2024-10-15 10:54:40 +0200

    Enlever Debug

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import MainApp as MApp
+import kivy
 from kivy.config import Config
+import logging
+
 Config.set('input', 'mouse', 'mouse,disable_multitouch')
+kivy.logger.Logger.setLevel(logging.WARNING)
+logging.getLogger('matplotlib').setLevel(logging.WARNING)
 
 if __name__ == '__main__':
     MApp.MainApp().run()
