6bcfd10b97cd61ca53826195f819827e8ab2ec7b
Clement Breau <clement.breau@etudiant.univ-lr.fr>
2024-10-17 09:10:28 +0200

    add doc to data_handler.py

--- data_handler.py
+++ data_handler.py
@@ -1,6 +1,23 @@
 import pandas as pd
 
 def createTableBlockedRequest(data):
+    """
+    Creates a pandas DataFrame for blocked requests based on provided data.
+
+    This function converts the provided data into a pandas DataFrame, removes any duplicate 
+    rows, and resets the index. If the DataFrame is empty or does not contain a 'date' column, 
+    it returns None. Otherwise, it returns a DataFrame with selected columns.
+
+    Args:
+        data (list of dict): A list of dictionaries where each dictionary contains information
+                             about a blocked request.
+
+    Returns:
+        pd.DataFrame or None: A DataFrame containing the 'date', 'table', 'utilisateur', 'id',
+                              and 'adresse' columns, or None if the DataFrame is empty or 
+                              does not contain a 'date' column.
+    """
+
     pd.set_option('display.max_columns', None)
     df = pd.DataFrame(data)
 
@@ -16,4 +33,17 @@ def createTableBlockedRequest(data):
 
 # not implemented yet
 def createCsvBlockedRequest(dataFrame):
+    """
+    Saves the provided DataFrame to a CSV file.
+
+    This function takes a pandas DataFrame and saves its contents to a CSV file 
+    named 'data.csv' without including the index in the output file.
+
+    Args:
+        dataFrame (pd.DataFrame): The DataFrame to be written to a CSV file.
+
+    Returns:
+        None
+    """
+
     dataFrame.to_csv('data.csv', index=False)
