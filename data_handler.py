41840f4a3dddd43a15322a2f449e060a30713efa
Coussy <leandre.coussy@etudiant.univ-lr.fr>
2024-12-09 14:59:58 +0100

    [FIX] Refactor DataHandler file

--- data_handler.py
+++ data_handler.py
@@ -1,11 +1,11 @@
 import pandas as pd
 
-def createTableBlockedRequest(data):
+def create_table_blocked_request(data):
     """
     Creates a pandas DataFrame for blocked requests based on provided data.
 
-    This function converts the provided data into a pandas DataFrame, removes any duplicate 
-    rows, and resets the index. If the DataFrame is empty or does not contain a 'date' column, 
+    This function converts the provided data into a pandas DataFrame, removes any duplicate
+    rows, and resets the index. If the DataFrame is empty or does not contain a 'date' column,
     it returns None. Otherwise, it returns a DataFrame with selected columns.
 
     Args:
@@ -14,29 +14,29 @@ def createTableBlockedRequest(data):
 
     Returns:
         pd.DataFrame or None: A DataFrame containing the 'date', 'table', 'utilisateur', 'id',
-                              and 'adresse' columns, or None if the DataFrame is empty or 
+                              and 'adresse' columns, or None if the DataFrame is empty or
                               does not contain a 'date' column.
     """
+    if not data:
+        return None
 
-    pd.set_option('display.max_columns', None)
     df = pd.DataFrame(data)
-
-    # remove all duplicate
     df.drop_duplicates(inplace=True)
-    df.reset_index(drop=True, inplace=True) # reset all index
-    if df.empty:
+    df.reset_index(drop=True, inplace=True)
+
+    if df.empty or 'date' not in df.columns:
         return None
-    if  not (df.columns.__contains__("date")):
-        return None
-    return df[['date','table','utilisateur' ,'id','adresse',]]
+
+    requiredColumns = ['date', 'table', 'utilisateur', 'id', 'adresse']
+    return df[requiredColumns] if all(col in df.columns for col in requiredColumns) else None
 
 
 # not implemented yet
-def createCsvBlockedRequest(dataFrame):
+def create_csv_blocked_request(dataFrame):
     """
     Saves the provided DataFrame to a CSV file.
 
-    This function takes a pandas DataFrame and saves its contents to a CSV file 
+    This function takes a pandas DataFrame and saves its contents to a CSV file
     named 'data.csv' without including the index in the output file.
 
     Args:
