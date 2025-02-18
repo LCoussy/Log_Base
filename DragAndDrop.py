8333f88ed7c3c117cab57dfae4f0c46b71535c9b
Leandre Coussy <leandre.coussy@etudiant.univ-lr.fr>
2025-02-18 14:08:11 +0100

    [TRY] Try to implement drag and drop feature with loading via threading

--- DragAndDrop.py
+++ DragAndDrop.py
@@ -2,16 +2,24 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.popup import Popup
+from kivy.uix.image import Image
 from kivy.uix.screenmanager import Screen
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.filechooser import FileChooserIconView
 from kivy.core.window import Window
 from kivy.properties import StringProperty
+from kivy.clock import Clock
+from kivy.app import App
 
+import concurrent.futures
 import threading
+import multiprocessing
 import os
+import time
 from processing import process_directory, validate_directory
 import GetContentLog
+import gc
+import tracemalloc
 
 Window.size = (1000, 600)
 
@@ -20,22 +28,65 @@ class DragDropScreen(Screen):
 
     def __init__(self, **kwargs):
         super(DragDropScreen, self).__init__(**kwargs)
-        self.parsingThread =threading.Thread(target=self.process_parsing)
+        self.parsingProcess = multiprocessing.Process(target=self.process_parsing, daemon=True)
         self.filesList = []
         self.build_ui()
     def build_ui(self):
         mainLayout = BoxLayout(orientation='horizontal')
+
         mainLayout.add_widget(self.create_left_layout())
         mainLayout.add_widget(self.create_right_layout())
         self.add_widget(mainLayout)
 
     def process_parsing(self):
-        for file in os.listdir(self.path):
-            if '/' in self.path:
-                GetContentLog.parse(self.path + '/' + file)
-            else :
-                GetContentLog.parse(self.path + '\\' + file)
 
+        class Loading(BoxLayout):
+            loader = Image(source="loading.gif", size_hint=(None, None), size=(200, 200), pos_hint={'center_x': 0.5, 'center_y': 0.5})
+            self.add_widget(loader)
+
+            loader.opacity = 1  # Affiche la barre de progression
+
+            liste_fichiers = [os.path.join(self.path, file) for file in os.listdir(self.path)]
+            chunk_size = 1000
+
+            def chunks(lst, n):
+                for i in range(0, len(lst), n):
+                    yield lst[i:i + n]
+
+            with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
+                for chunk in chunks(liste_fichiers, chunk_size):
+                    # executor.map(self.process_parsing_one_file, chunk)
+                    # Libérer la mémoire et vider le cache
+                    # tracemalloc.start()
+                    # snapshot1 = tracemalloc.take_snapshot()
+
+                    # Process files in the chunk
+                    executor.map(self.process_parsing_one_file, chunk)
+
+                    # snapshot2 = tracemalloc.take_snapshot()
+                    # top_stats = snapshot2.compare_to(snapshot1, 'lineno')
+
+                    # print("[ Top 10 differences ]")
+                    # for stat in top_stats[:10]:
+                    #     print(stat)
+
+                    # time.sleep(15)
+
+                    executor.shutdown(wait=True)
+                    executor = concurrent.futures.ThreadPoolExecutor(max_workers=4)
+                    gc.collect()
+
+            loader.opacity = 0  # Cache la barre de progression une fois terminé
+
+        class MyApp(App):
+            def build(self):
+                return Loading()
+
+        MyApp().run()
+
+
+    def process_parsing_one_file(self, filepath):
+        GetContentLog.parse(filepath)
 
     def create_left_layout(self):
         leftLayout = BoxLayout(orientation='vertical', size_hint=(0.3, 1), padding=10, spacing=10)
@@ -106,7 +157,7 @@ class DragDropScreen(Screen):
         #         GetContentLog.parse(self.path + '/' + file)
         #     else :
         #         GetContentLog.parse(self.path + '\\' + file)
-        self.parsingThread.start()
+        self.parsingProcess.start()
         Screen = self.manager.get_screen('display')
         Screen.logExplorer.update_directory(self.path)
         self.manager.current = 'display'
--- MainApp.py
+++ MainApp.py
@@ -1,6 +1,7 @@
 #MainApp.py
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager
+from kivy.uix.image import Image
 
 from DragAndDrop import DragDropScreen
 from Display import Display
@@ -9,6 +10,8 @@ class MainApp(App):
     def build(self):
         # Display Block screen
         sm = ScreenManager()
+
+
         dragDropScreen = DragDropScreen(name='drag_drop')
         sm.add_widget(dragDropScreen)
 
